229c3360079817cf9cb0f2f002ab0f73771aa507
connorbroad <connor.broad@gmail.com>
2025-08-24 20:59:24 +1200

    Url test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/GithubPagesTest/',
   integrations: [svelte()],
-  server: { port: 4200 },
-  base: '/GithubPagesTest/'
+  server: { port: 4200 }
 });
